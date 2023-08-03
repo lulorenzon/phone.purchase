354a9fc560a908b77b276bb3c1b7cbcad050cb76
luana.santos <luana.santos@g.globo>
2023-08-02 21:54:28 -0300

    feat: add js

---
+++ phone-purchase/main.js
@@ -0,0 +1,10 @@
+const buttonId = document.querySelector("button")
+
+
+buttonId.addEventListener("click", function(){
+    console.log("Fui clicado")
+})
+// function dizTeste(){
+//     console.log("Fui clicado")
+
+// }
